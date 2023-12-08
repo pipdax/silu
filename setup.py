55bc2f9e62378b4f5f563932a3a5f3c35b72bc57
pipdax <37277918+pipdax@users.noreply.github.com>
2023-12-08 16:05:05 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,22 @@
-#!/usr/bin/env python
-
 import setuptools
 
-if __name__ == "__main__":
-    setuptools.setup()
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="silu", 
+    version="0.0.1",
+    author="Dai Hongbin", 
+    author_email="pipdax@126.com", 
+    description="SILU, a new way to develop risk models.",
+    long_description=long_description, 
+    long_description_content_type="text/markdown",
+    url="https://github.com/pipdax/silu",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
